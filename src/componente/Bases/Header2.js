a166e2c6afe54846fb942db0f91e098a409003c4
JESSICA PEREIRA DOS SANTOS <23111734@dsn127m1242374.cfp127aluno.local>
2024-09-24 17:26:14 -0300

    adicionar escolha de cadastro

---
+++ src/componente/Bases/Header2.js
@@ -0,0 +1,23 @@
+import image from '../img/Logo.png'
+import styles from '../Css/header.module.css'
+import { useNavigate } from 'react-router-dom'
+
+function Header2() {
+    const navigate = useNavigate();
+    return (
+        <div className={styles.header}>
+            <img src={image}></img>
+
+            <div className={styles.list}>
+                <div className={styles.item}>
+                    Já possui conta? <button className={styles.item} onClick={() => navigate('/Login')}> Entrar</button>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header2
--- src/componente/Pages/Cadastro.js
+++ src/componente/Pages/Cadastro.js
@@ -1,11 +1,37 @@
-// import styles from '../Css/cadastro.module.css'
+import styles from '../Css/cadastro.module.css'
+import Header2 from '../Bases/Header2'
+import imgFrelancer from '../img/freelancer.png'
+import imgCliente from '../img/cliente.png'
 
 
-function Cadastro (){
-    return(
+function Cadastro() {
+    return (
         <div>
+            <Header2 />
+            <div className={styles.tela}>
+                <h1 className={styles.h1}>Cadastrar-se como um cliente ou como um freelancer?</h1>
 
+                <div className={styles.escolhas}>
+                    <div className={styles.escolha1}>
+                        <img src={imgFrelancer}></img>
+                        <p>wibuvisbuv</p>
+                    </div>
+
+                    <div className={styles.escolha2}>
+                        <img src={imgCliente}></img>
+                        <p>wibuvisbuv</p>
+                    </div>
+
+                </div>
+
+                <button className={styles.botao}>
+                    Criar uma conta
+                </button>
+
+            </div>
         </div>
+
+
     )
 }
 
