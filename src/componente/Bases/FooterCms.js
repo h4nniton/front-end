ec3c32525819a9ada61e7c2ffda0cd73329fd363
JINNIELABORE <elaborejinni@gmail.com>
2024-09-26 17:25:41 -0300

    adicionar

--- src/componente/Bases/FooterCms.js
+++ src/componente/Bases/FooterCms.js
@@ -1,10 +1,13 @@
 import EventoCadastroCms from '../Eventos/EventoCadastroCms'
 import styles from '../Css/footer.module.css'
+import { useNavigate } from 'react-router-dom'
+
 
 function FooterCms (){
+    const navigate = useNavigate();
     return(
         <div>
-            <p> já possui conta? <p className={styles.link}> Entrar</p></p>
+            <p> já possui conta? <p className={styles.link} onClick={()=>navigate('/Login')}> Entrar</p></p>
             <EventoCadastroCms />
 
         </div>
--- src/componente/Bases/FooterFreelancer.js
+++ src/componente/Bases/FooterFreelancer.js
@@ -1,10 +1,13 @@
 import EventoCadastroFreelancer from '../Eventos/EventoCadastroFreelancer'
 import styles from '../Css/footer.module.css'
+import { useNavigate } from 'react-router-dom'
+
 
 function Footer (){
+    const navigate = useNavigate();
     return(
         <div>
-            <p> já possui conta? <p className={styles.link}> Entrar</p></p>
+            <p> já possui conta? <p className={styles.link} onClick={()=>navigate('/Login')}> Entrar</p></p>
             <EventoCadastroFreelancer />
 
         </div>
