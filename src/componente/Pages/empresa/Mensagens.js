fe1be72e23e789cd3ef1bfa9488287ce483b373a
JINNIELABORE <elaborejinni@gmail.com>
2024-11-19 17:18:35 -0300

    adicionar

--- src/componente/Pages/empresa/Mensagens.js
+++ src/componente/Pages/empresa/Mensagens.js
@@ -1,12 +1,17 @@
 import HeaderOficial from '../../Bases/HeaderOficial.js'
 import NavBar from '../../Bases/NavBar2.js'
+import Chat from "../chat/Chat.js"
+import Style from "../../Css/chat.module.css"
 
 function Mensagens (){
     return (
         <div>
             <HeaderOficial/>
-            <div>
+            <div className={Style.tela}>
                 <NavBar/>
+                <div className={Style.Chat}>
+                <Chat/>
+                </div>
             </div>
 
         </div>
