4cdec5f3dbfce8c5bec66e340e5f1522634ee096
jeca16 <annaeelza@gmail.com>
2024-11-27 01:36:16 -0300

    grafico +/- funcional

--- src/componente/Pages/empresa/Portfolio.js
+++ src/componente/Pages/empresa/Portfolio.js
@@ -2,58 +2,52 @@ import img from '../../img/exemplo portifolio.png'
 import Style from '../../Css/portifolio.module.css'
 
 
-function Portfolio() {
+function Portfolio({ listPortfolio }) {
+    const verificarImagem = (url) => {
+        return new Promise((resolve) => {
+          const img = new Image();
+          img.onload = () => resolve(true);  
+          img.onerror = () => resolve(false); 
+          img.src = url; 
+        });
+      };
+    
+      const ImagemPortfolio = ({ url, defaultImage }) => {
+        const [isValidImage, setIsValidImage] = useState(false);
+    
+        useEffect(() => {
+          const verificar = async () => {
+            const resultado = await verificarImagem(url);
+            setIsValidImage(resultado);  
+          };
+    
+          verificar(); 
+        }, [url]); 
+        return (
+          <img
+            src={isValidImage ? url : defaultImage}
+            alt="Imagem do portfólio"
+          />
+        );
+      };
+    
+      const defaultImage = "/src/componente/img/iconzip.png";
     return (
         <div className={Style.tela}>
             <div className={Style.historico}>
                 <div className={Style.previa}>
-                    <div className={Style.portfolio}>
-                        <img src={img}></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit lorem quam. Aenean et urna et nibh posuere suscipit. Sed gravida</p>
-                    </div>
-
-                    <div className={Style.portfolio}>
-                        <img src={img}></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit lorem quam. Aenean et urna et nibh posuere suscipit. Sed gravida</p>
-                    </div>
-
-                    <div className={Style.portfolio}>
-                        <img src={img}></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit lorem quam. Aenean et urna et nibh posuere suscipit. Sed gravida</p>
-                    </div>
+                    {listPortfolio && listPortfolio.length > 0 ? (
+                        listPortfolio.map((portfolio, index) => (
+                            <div key={index} className={Style.portfolio}>
+                                <ImagemPortfolio url={portfolio.arquivo} defaultImage={defaultImage} />
+                            </div>
+                        ))
+                    ) : (
+                        <p>not found</p>
+                    )}
                 </div>
-                <div className={Style.previa}>
-                    <div className={Style.portfolio}>
-                        <img src={img}></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit lorem quam. Aenean et urna et nibh posuere suscipit. Sed gravida</p>
-                    </div>
-
-                    <div className={Style.portfolio}>
-                        <img src={img}></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit lorem quam. Aenean et urna et nibh posuere suscipit. Sed gravida</p>
-                    </div>
-
-                    <div className={Style.portfolio}>
-                        <img src={img}></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit lorem quam. Aenean et urna et nibh posuere suscipit. Sed gravida</p>
-                    </div>
-                </div>
-                <div className={Style.previa}>
-                    <div className={Style.portfolio}>
-                        <img src={img}></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit lorem quam. Aenean et urna et nibh posuere suscipit. Sed gravida</p>
-                    </div>
-
-                    <div className={Style.portfolio}>
-                        <img src={img}></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit lorem quam. Aenean et urna et nibh posuere suscipit. Sed gravida</p>
-                    </div>
-
-                    <div className={Style.portfolio}>
-                        <img src={img}></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit lorem quam. Aenean et urna et nibh posuere suscipit. Sed gravida</p>
-                    </div>
-                </div>
+                
+               
             </div>
         </div>
     )
